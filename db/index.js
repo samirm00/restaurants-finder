e9a039b489de8b46018fb09180dc9df0280de573
samirm00 <sammoham.be@gmail.com>
2021-03-25 20:50:44 +0100

    delpoy2

--- db/index.js
+++ db/index.js
@@ -5,3 +5,10 @@ const pool = new Pool();
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
+
+/*
+
+psql --host=ec2-54-161-239-198.compute-1.amazonaws.com --port=5432 --username=oonxsikiusxlvk --password --dbname=d48k11forn8qhq
+
+
+*/
