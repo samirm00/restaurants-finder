10a2c93b99035e67704ba9b2f395817caa8f20ce
Samir M <67711948+samirm00@users.noreply.github.com>
2021-03-25 21:16:11 +0100

    Update index.js

--- db/index.js
+++ db/index.js
@@ -6,9 +6,3 @@ module.exports = {
   query: (text, params) => pool.query(text, params),
 };
 
-/*
-
-psql --host=ec2-54-161-239-198.compute-1.amazonaws.com --port=5432 --username=oonxsikiusxlvk --password --dbname=d48k11forn8qhq
-
-
-*/
