f6d11ec0dea2bc90496f73c7e09ff049fcafe09f
samirm00 <sammoham.be@gmail.com>
2021-05-04 23:08:19 +0200

    ready to deploy3

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "/api/v1/restaurants"
+    ? "https://still-castle-10933.herokuapp.com/api/v1/restaurants"
     : "http://localhost:5000/api/v1/restaurants";
 export default axios.create({
   baseURL,
