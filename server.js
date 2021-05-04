7b3b7e1244ecbb7f74a7e062b9f7fe2d8bf80f70
samirm00 <sammoham.be@gmail.com>
2021-05-04 23:03:31 +0200

    ready to deploy2

--- server.js
+++ server.js
@@ -252,6 +252,16 @@ app.post("/api/v1/restaurants/:id/addReview", (req, res) => {
   });
 });
 
+// production
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server is up and listening on port ${port}`);
